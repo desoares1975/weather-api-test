ed89ac5ca50319152f0618f484cdaf8da800ac9b
desoares <fabio.desoares@gmail.com>
2018-09-16 13:15:07 -0300

    listing routes incomplete

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,22 @@
 'use strict'
 
+const data = require('../../data')
+
 module.exports = app => {
   app.get('/', (req, res) => res.status(200).json('WELLCOME'))
+  app.get('/cities', (req, res) => res.status(200).json(data.cities))
+  app.get('/cities/weather', (req, res) => {
+    let weathers = []
+    let weatheredCities = data.weather.map(w => {
+      weathers.push({ [w.cityId]: w })
+      return w.cityId
+    })
+    let response = data.cities
+      .filter(city => weatheredCities.includes(city.id))
+      .map(city => {
+        city.weather = weathers[city.id]
+        return city
+      })
+    res.status(200).json(response)
+  })
 }
