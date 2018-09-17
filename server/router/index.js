7d6e4680de994571876eea31aa5bf843f83499a0
desoares <fabio.desoares@gmail.com>
2018-09-17 07:45:17 -0300

    weathered cities complete

--- server/router/index.js
+++ server/router/index.js
@@ -6,14 +6,17 @@ module.exports = app => {
   app.get('/', (req, res) => res.status(200).json('WELLCOME'))
   app.get('/cities', (req, res) => res.status(200).json(data.cities))
   app.get('/cities/weather', (req, res) => {
-    let weathers = []
+    let weathers = {}
     let weatheredCities = data.weather.map(w => {
-      weathers.push({ [w.cityId]: w })
+      weathers[w.cityId] = w.data
       return w.cityId
     })
+
+    console.log()
     let response = data.cities
       .filter(city => weatheredCities.includes(city.id))
       .map(city => {
+        console.log(weathers[city.id], city.id)
         city.weather = weathers[city.id]
         return city
       })
